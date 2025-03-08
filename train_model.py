ba0d1131edbca37d736784cc6cd8545142c67710
yudai-22 <progaress21@gmail.com>
2025-03-08 20:10:50 +0900

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
+import wandb
 
 from training_sub import DataSet, EarlyStopping
 
